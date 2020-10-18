87c3e25b2b2da1dfb89175501ed87025aacde61e
haovipaws <darkclawsthenightwing@gmail.com>
2020-10-18 16:21:24 -0700

    make boxes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
 use std::io;
 use tui::Terminal;
 use tui::backend::CrosstermBackend;
+use tui::widgets::{Widget, Block, Borders};
+use tui::layout::{Layout, Constraint, Direction};
 
 fn main() -> Result<(), io::Error> {
     let stdout = io::stdout();
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
-    Ok(())
+    terminal.draw(|f| {
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(0)
+            .constraints(
+                [
+                    Constraint::Percentage(10),
+                    Constraint::Percentage(80),
+                    Constraint::Percentage(10)
+                ].as_ref()
+            )
+            .split(f.size());
+        let block = Block::default()
+             .title("Block")
+             .borders(Borders::ALL);
+        f.render_widget(block, chunks[0]);
+        let block = Block::default()
+             .title("Block 2")
+             .borders(Borders::ALL);
+        f.render_widget(block, chunks[1]);
+    })
 }
